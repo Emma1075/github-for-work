394cfd923b72d3035de102c8d18f4450019a44e9
sugar <1075383926@qq.com>
2017-05-04 10:23:12 +0800

    ie 兼容性问题 dataset and css

--- adultEducation/js/getRequest.js
+++ adultEducation/js/getRequest.js
@@ -1,3 +1,79 @@
+// ie: arr.forEach
+if (typeof Array.prototype.forEach != "function") {
+  Array.prototype.forEach = function (fn, scope) {
+    var i, len;
+    for (i = 0, len = this.length; i < len; ++i) {
+      if (i in this) {
+        fn.call(scope, this[i], i, this);
+      }
+    }
+  }
+};
+
+// ie: object.assign
+if (typeof Object.assign != 'function') {
+  (function () {
+    Object.assign = function (target) {
+     'use strict';
+     if (target === undefined || target === null) {
+       throw new TypeError('Cannot convert undefined or null to object');
+     }
+    
+     var output = Object(target);
+     for (var index = 1; index < arguments.length; index++) {
+       var source = arguments[index];
+       if (source !== undefined && source !== null) {
+         for (var nextKey in source) {
+           if (source.hasOwnProperty(nextKey)) {
+             output[nextKey] = source[nextKey];
+           }
+         }
+       }
+     }
+     return output;
+    };
+  })();
+}
+
+// 兼容 ie 事件
+function addEvent(ele, type, fn) {
+  if (ele.addEventListener) {
+    ele.addEventListener(type, fn, false);
+  } else if(ele.attachEvent) {
+    ele.attachEvent('on' + type, fn)
+  }
+}
+
+// 兼容 ie dataset
+function get_dataset(ele){  
+  if(ele.dataset){
+    return ele.dataset;  
+  } else {  
+      //兼容代码  
+      var dataset = {};  
+      var ele_split = ele.outerHTML.split(" ");  
+      for(var i = 0,element; i < ele_split.length; i++) {  
+         element = ele_split[i];  
+         if (element.substring(0,4) == "data") {   
+             if (element.indexOf(">") !=  -1) {   
+                 element = element.split(">")[0];  
+             };  
+             ele_key=element.split("=")[0].slice(5);  
+             ele_value=element.split("=")[1].slice(1,-1);  
+             if(ele_key.indexOf("-") ==  -1){  
+                 dataset[ele_key] = ele_value;  
+             }else{  
+                 ele_keys=ele_key.split("-");  
+                 ele_key=ele_keys[0];  
+                 for(i=1;i<ele_keys.length;i++){  
+                     ele_key+=replaceReg(ele_keys[i]);  
+                 }                 
+             }  
+         };  
+      }  
+      return dataset;  
+  }  
+}
 // 设置 线上地址
 var onlineURL = 'http://static.cj.chinaedu.net/web-public-static/rsc/';
 
@@ -9,6 +85,7 @@ function getParameters() {
   var strToArr = str.split('&');
   var obj = {};
   // 将参数放在obj对象中
+
   strToArr.forEach(function(ele) {
     var tempArr = ele.split('=');
     obj[tempArr[0]] = tempArr[1];
@@ -60,3 +137,4 @@ function myBrowser () {
     return false;
   }
 }
+
